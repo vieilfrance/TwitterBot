72307862d49f2c3b1488e1c89db94ace3e71aa29
David Beck <dbec.pro@gmail.com>
2014-07-13 22:54:11 +0200

    datetime in log, adjust probability of replies, ...

--- param.py
+++ param.py
@@ -1,17 +1,19 @@
 ﻿LATESTMFILE = 'last_id.txt'
 LOGFILE = "twitterbot_log.txt"
+verbose = False
+twitterName = "ui_cer_bot"
 
 # Liste de terme qui servent pour répondre
 answers = ['ahah :)' , 'YO' , 'O_O', 'stoi' , 'TG' , 'MER IL ET FOU']
 
 # Liste des terme qui servent a repondre "stoi xxxx"
-bad_words = {'boloss' : 'le boloss', 'boulette' : 'la boulette', 'accident' :"l'accident" , 'youtube':"le tube"}
+bad_words = {'boloss' : 'le boloss', 'boulette' : 'la boulette', 'accident' :"l'accident" , 'youtube':"le tube" , 'facebook':"le bouc" , 'dément': "qui ment"}
 
 # Liste des terme relou ou le bot repond TG avec un mention paritculiere pour @infredwetrust :)
-boring_words = {'#old' , 'oscours', '#oscours' }
+boring_words = {'#old' , 'oscours', '#oscours', "twitpic", "selfie" }
 
 # Liste des termes qui enclenche une reponse
 tg_list = ['tg','ta gueule', 'tg.', 'tg!', 'ta gueule.', 'ta gueule!']
 
 #Liste des phrase que le bot tweete de lui-emme
-talk = {"c'est l'amour à la plage, aoum tcha tcha tcha", "Je vous trouve très beau, surtout moi" , "y a quoi de beau à la télé ce soir ?", "sim est mort. #rip"}
+talk = {"Sinon SAVA ?", "c'est l'amour à la plage, aoum tcha tcha tcha", "Je vous trouve très beau, surtout moi" , "y a quoi de beau à la télé ce soir ?", "sim est mort. #rip"}
--- twitterBot.py
+++ twitterBot.py
@@ -19,10 +19,11 @@ def takeDecision(proba, base):
 		
 # fonction de log
 def log(txt,type="INFO"):
-	txt="\n"+type+" - "+txt
-	fp = open(LOGFILE, 'a')
-	fp.write(str(txt))
-	fp.close()
+	if (verbose==True or type !="INFO") :
+		txt="\n"+time.strftime('%d/%m/%y %H:%M',time.localtime())+" : "+type+" - "+txt
+		fp = open(LOGFILE, 'a')
+		fp.write(str(txt))
+		fp.close()
 
 ########################################################################################################
 
@@ -39,28 +40,26 @@ if os.path.exists(LATESTMFILE):
 else:
 	lastid = 0
 	
-# Get your "home" timeline and insult someone 
+# Get your "home" timeline
 tl=twitter.statuses.home_timeline(since_id=lastid)
 log("Parse the timeline")
 for tweet in tl:
-	#print tweet['user']['screen_name'].encode('ascii', 'replace')
-	#print tweet['text'].lower().encode('ascii', 'replace')
-	#print tweet
 	if tweet['id'] not in repliedtweetsits :
-		
 		log("Section badwords")
 		match = set(bad_words) & set((tweet['text'].lower().encode('ascii', 'replace')).split())
-		if match and 'ui_cer_bot' not in tweet['user']['screen_name'].encode('ascii', 'replace'):
+		#check if there is a badword in someone's tweets
+		if match and twitterName not in tweet['user']['screen_name'].encode('ascii', 'replace'):
 			badword=match.pop().upper()
 			reply=random.sample(answers,1).pop()
 			log("match avec %s" % (badword))
-			if takeDecision(50,100) :
+			if takeDecision(70,100) :
+			# reply if match
 				if reply == 'stoi' :
-					#print "@%s stoi %s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()]) , (badword))
 					try :
 						twitter.statuses.update(status="@%s stoi %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()])),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("@%s stoi %s " % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()])),"TWEET")
+						time.sleep(1)
 					except :
 						log("@%s stoi %s " % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()])),"WARNING")
 				else :
@@ -68,39 +67,41 @@ for tweet in tl:
 						twitter.statuses.update(status="@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),"TWEET")
+						time.sleep(1)
 					except :
 						log("@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),"WARNING")					
 
 		log("Section Boring")
+		#check if there is a boring word in someone's tweets
 		match = set(boring_words) & set((tweet['text'].lower().encode('ascii', 'replace')).split())
-		if match and '@ui_cer_bot' not in tweet['text'].lower() :
+		if match and '@'+twitterName not in tweet['text'].lower() :
 			log("match avec %s" % (match))
 			if takeDecision(60,100) :
 				if tweet['user']['screen_name'].encode('ascii', 'replace') == 'infredwetrust' :
-					#print "@%s TGF" % (tweet['user']['screen_name'].encode('ascii', 'replace'))
 					try :
 						twitter.statuses.update(status="@%s TGF" % (tweet['user']['screen_name'].encode('ascii', 'replace')),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("@%s TGF" % (tweet['user']['screen_name'].encode('ascii', 'replace')),"TWEET")
+						time.sleep(1)
 					except :
 						log("@%s TGF" % (tweet['user']['screen_name'].encode('ascii', 'replace')),"WARNING")
 				else :
-					#print "TG @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace'))
 					try :
 						twitter.statuses.update(status="TG @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace')),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("TG @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace')),"TWEET")
+						time.sleep(1)
 					except :
 						log("TG @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace')),"WARNING")
 				
 		
 		log("Section OSEF")
-		if takeDecision(1,100) :
-			#print "OSEF @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace'))
+		if takeDecision(5,100) :
 			try :
 				twitter.statuses.update(status="OSEF @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace')) ,in_reply_to_status_id=tweet['id'])
 				repliedtweetsits.append(tweet['id'])
 				log("OSEF @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace')),"TWEET")
+				time.sleep(1)
 			except :
 				log("OSEF @%s " % (tweet['user']['screen_name'].encode('ascii', 'replace')),"WARNING")
 		
@@ -109,22 +110,20 @@ for tweet in tl:
 mt=twitter.statuses.mentions_timeline(since_id=lastid)
 log("Parse the mentions")
 for tweet in mt:
-	#print tweet['user']['screen_name'].encode('ascii', 'replace')
-	#print tweet['text'].lower().encode('ascii', 'replace')
 	log("Section Mentions")
 	if tweet['id'] not in repliedtweetsits :
 		match = set(tg_list) & set((tweet['text'].lower().encode('ascii', 'replace')).split())
-		if match and 'ui_cer_bot' not in tweet['user']['screen_name'].encode('ascii', 'replace') :
+		if match and twitterName not in tweet['user']['screen_name'].encode('ascii', 'replace') :
 			badword=match.pop().upper()
 			reply=random.sample(answers,1).pop()
 			log("match avec %s" % (reply))
 			if takeDecision(90,100) :	
 				if reply == 'stoi' :
-					#print "@%s stoi %s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()]) , (badword))
 					try :
 						twitter.statuses.update(status="@%s stoi %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()])),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("@%s stoi %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()])),"TWEET")
+						time.sleep(1)
 					except :
 						log("@%s stoi %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()])),"WARNING")
 				else :
@@ -132,26 +131,27 @@ for tweet in mt:
 						twitter.statuses.update(status="@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),"TWEET")
+						time.sleep(1)
 					except :
 						log("@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),"WARNING")					
 			elif takeDecision(1,100) :
 				log("On laisse passer le tweet" ,"TWEET")
 				repliedtweetsits.append(tweet['id'])
-		time.sleep(1)
+
 
 		log("Section badwords")
 		match = set(bad_words) & set((tweet['text'].lower().encode('ascii', 'replace')).split())
-		if match and 'ui_cer_bot' not in tweet['user']['screen_name'].encode('ascii', 'replace') :
+		if match and twitterName not in tweet['user']['screen_name'].encode('ascii', 'replace') :
 			badword=match.pop().upper()
 			reply=random.sample(answers,1).pop()
 			log("match avec %s" % (badword))
 			if takeDecision(50,100) :
 				if reply == 'stoi' :
-					#print "@%s stoi %s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()]) , (badword))
 					try :
 						twitter.statuses.update(status="@%s stoi %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()]) , (badword)),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("@%s stoi %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()]) ),"TWEET")
+						time.sleep(1)
 					except :
 						log("@%s stoi %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (bad_words[badword.lower()]) ),"WARNING")
 				else :
@@ -159,6 +159,7 @@ for tweet in mt:
 						twitter.statuses.update(status="@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),in_reply_to_status_id=tweet['id'])
 						repliedtweetsits.append(tweet['id'])
 						log("@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),"TWEET")
+						time.sleep(1)
 					except :
 						log("@%s %s" % (tweet['user']['screen_name'].encode('ascii', 'replace') , (reply) ),"WARNING")					
 			elif takeDecision(1,100) :
@@ -167,7 +168,7 @@ for tweet in mt:
 
 log("Section Ranom Tweet")
 reply=random.sample(talk,1).pop()
-if takeDecision(10,100) :
+if takeDecision(15,100) :
 	try :
 		twitter.statuses.update(status="%s" % (reply) ,in_reply_to_status_id=tweet['id'])
 		log("%s" % (reply),"TWEET")
